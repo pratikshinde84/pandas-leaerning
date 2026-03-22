239e641cddc3d6636c3ff298987889075f32c925
Pratik <pratik216029@gmail.com>
2026-03-22 21:16:25 +0530

    done

---
+++ plotting/one.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+df=pd.read_csv("medicine.csv")
+print(df.head())
+import matplotlib.pyplot as plt
+x=[1,2,3,4]
+y=[10,20,25,15]
+plt.plot(x,y)
+plt.xlabel("ages")
+plt.ylabel("numbers")
+plt.show()
